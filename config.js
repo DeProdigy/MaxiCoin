b0d38c02ed6c7ddd96dbd1a147a9a50bf68d1745
DeProdigy <alex.hint.ah@gmail.com>
2022-07-29 18:06:09 -0400

    New deploy

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
-export const maxiCoinAddress = "0x2bc23651613b2737ad9F7d7C125750177eA21DCA" 
-export const nftAddress = "0x2639A57D434483665Ceb55B4A146800a0da1ce54"
+export const maxiCoinAddress = "0x14346C2f204F4929D694bB76d58f19C5D9bb7c23" 
+export const nftAddress = "0x7C328d9B57eD08169E29f8b4FBc70e3887a39d1c" 
+export const maxAddress="0xd472e7FE0CEBb1F4085071dF239e51129CF4A39c"
